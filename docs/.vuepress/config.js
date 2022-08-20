14fb8bbdae6668ddbf25581817d075dc8c091d92
xiexiong <2579873098@qq.com>
2022-08-20 10:57:34 +0800

    --docs

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,7 +4,7 @@ module.exports = {
     lang: 'zh-CN',
     title: '面试题收集',
     description: '收录了常见的面试题，希望能帮助到你',
-    base: '/interview/',
+    base: './',
     theme: '@vuepress/theme-default',
     themeConfig: {
         displayAllHeaders: true,
